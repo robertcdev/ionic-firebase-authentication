667ba3416b78881b75c2235a61752290bcac77eb
Robert Collier <robertcdev@gmail.com>
2019-12-23 01:35:22 -0800

    Implement firebase auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public platform: Platform
+  ) { }
+
+  doRegister(value) {
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().createUserWithEmailAndPassword(value.email, value.password).then(
+        res => resolve(res),
+        err => reject(err)
+      )
+    });
+  }
+ 
+  doLogin(value) {
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(value.email, value.password).then(
+        res => resolve(res),
+        err => reject(err)
+      )
+    });
+  }
+ 
+  doLogout() {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.signOut()
+      .then(() => {
+        resolve();
+      })
+      .catch((error) => {
+        console.log(error);
+        reject();
+      });
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAr7D2JbkzG4nmuQysQdeyAsqLvgxXWY8U",
+    authDomain: "zoe-v1-19ba3.firebaseapp.com",
+    databaseURL: "https://zoe-v1-19ba3.firebaseio.com",
+    projectId: "zoe-v1-19ba3",
+    storageBucket: "zoe-v1-19ba3.appspot.com",
+    messagingSenderId: "905702830729",
+    appId: "1:905702830729:web:2d9f5fd099d717b82acc58",
+    measurementId: "G-7PD0F9VYHL"
+  },
+  googleWebClientId: '905702830729-dfoj89ufoc6btdfi87gqsal10p968vdc.apps.googleusercontent.com'
 };
 
 /*
