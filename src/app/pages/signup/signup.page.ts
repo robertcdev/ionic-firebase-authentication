a204f9f2f088f1ea5b6eca0d973fadf63fb5622a
Robert Collier <robertcdev@gmail.com>
2019-12-23 01:25:28 -0800

    Integrate form group into signup page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+      name: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ]))
+    });
+  }
+
+  tryRegister(value) {
+    // 
+  }
+
+  tryFacebookLogin() {
+    // 
+  }
+
+  tryGoogleLogin() {
+    // 
+  }
+
+  tryTwitterLogin() {
+    // 
   }
 
 }
