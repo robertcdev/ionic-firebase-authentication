516e0fc2ecd44b0ed0f273fe1d47c627b874b72d
Robert Collier <robertcdev@gmail.com>
2019-12-23 23:07:20 -0800

    Implement tabs page title

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  page_title: string;
 
+  constructor(
+    private router: Router
+  ) { }
+
+  setTitle() {
+    switch (this.router.url) {
+      case '/tabs/home':
+        this.page_title = 'Home';
+        break;
+      case '/tabs/shopping-list':
+        this.page_title = 'Shopping List';
+        break;
+      case '/tabs/recipes':
+        this.page_title = 'Recipes';
+        break;
+      case '/tabs/profile':
+        this.page_title = 'Profile';
+        break;
+      default:
+        this.page_title = 'Home';
+        break;
+    }
+  }
 }
