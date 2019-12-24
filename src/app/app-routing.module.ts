ed70fae03e7520be9ae7abc87dd972f79b41ba18
Robert Collier <robertcdev@gmail.com>
2019-12-23 16:20:10 -0800

    Integrate auth guard to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'welcome',
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    public afAuth: AngularFireAuth
+  ) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return new Promise((resolve, reject) => {
+      this.afAuth.user.subscribe(user => {
+        if (user) {
+          resolve(true);
+        } else {
+          console.log('User is not logged in');
+          this.router.navigate(['/welcome']);
+          resolve(false);
+        }
+      });
+    });
   }
   
 }
